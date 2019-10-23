8533bea57c3598f0526068df1ce8c075c408df77
Alif Taufik <alif.tr.aminuddin@gmail.com>
2019-10-23 15:39:13 +0700

    done creating transaction products

--- migrations/20191023040435-create-subscription-product.js
+++ migrations/20191023040435-create-subscription-product.js
@@ -1,30 +1,33 @@
 'use strict';
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('SubscriptionProducts', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      SubscriptionId: {
-        type: Sequelize.INTEGER
-      },
-      ProductId: {
-        type: Sequelize.INTEGER
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('SubscriptionProducts');
-  }
+	up: (queryInterface, Sequelize) => {
+		return queryInterface.createTable('SubscriptionProducts', {
+			id: {
+				allowNull: false,
+				autoIncrement: true,
+				primaryKey: true,
+				type: Sequelize.INTEGER
+			},
+			SubscriptionId: {
+				type: Sequelize.INTEGER
+			},
+			ProductId: {
+				type: Sequelize.INTEGER
+			},
+			amount: {
+				type: Sequelize.INTEGER
+			},
+			createdAt: {
+				allowNull: false,
+				type: Sequelize.DATE
+			},
+			updatedAt: {
+				allowNull: false,
+				type: Sequelize.DATE
+			}
+		});
+	},
+	down: (queryInterface, Sequelize) => {
+		return queryInterface.dropTable('SubscriptionProducts');
+	}
 };
